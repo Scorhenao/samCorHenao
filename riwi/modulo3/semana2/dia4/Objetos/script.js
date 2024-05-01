d1d6b993d38ad7c827f1191d083f41c54c7eaf40
Scorhenao <samcorhenao@gmail.com>
2024-04-30 20:18:45 -0500

    fix projcts

--- riwi/modulo3/semana2/dia4/Objetos/script.js
+++ riwi/modulo3/semana2/dia4/Objetos/script.js
@@ -6,30 +6,30 @@ console.log(boton);
 
 //Objeto
 
-let persona = {
-    nombre: "Juan",
-    apellido: "McKinley",
+// let persona = {
+//     nombre: "Juan",
+//     apellido: "McKinley",
 
-    edad: 25,
-    profesion: "Desarrollador",
+//     edad: 25,
+//     profesion: "Desarrollador",
 
-    //poniendo funcion
-    saludar: () =>{
-        return`hola soy ${this.nombre} y tengo ${this.edad} años.`;
-    },
+//     //poniendo funcion
+//     saludar: () =>{
+//         return`hola soy ${this.nombre} y tengo ${this.edad} años.`;
+//     },
 
-    toString: () =>{
-        return console.log(`${this.nombre}, ${this.apellido}, ${this.edad} años, ${this.profesion}`);
-    }//poninedo todo a string
-};
+//     toString: () =>{
+//         return console.log(`${this.nombre}, ${this.apellido}, ${this.edad} años, ${this.profesion}`);
+//     }//poninedo todo a string
+// };
 
 
-console.log(persona.nombre);
-persona.saludar();
-console.log(persona['profesion']);
+// console.log(persona.nombre);
+// persona.saludar();
+// console.log(persona['profesion']);//acceder a propiedad
 
-let propiedad = 'mascota';//acceder a una propiedad sin usar funciones
-console.log(persona[propiedad]);//no quemar el objeto, buena practica q acceda a propiedad
+// let propiedad = 'mascota';//acceder a una propiedad sin usar funciones
+// console.log(persona[propiedad]);//no quemar el objeto, buena practica q acceda a propiedad
 
 
 //console.log(persona.saludar());//"error si ya hay un console.log()" si lo queremos arreglar debemos poner return `hola soy ${this.nombre} y tengo ${this.edad} años.`
@@ -38,38 +38,38 @@ console.log(persona[propiedad]);//no quemar el objeto, buena practica q acceda a
 
 //Agregando al objeto
 
-persona.calificacion = "alta";
+// persona.calificacion = "alta";
 
-persona[propiedad] = "Pedro";//la variable propiedad se vuelve una propiedad como su nombre los dice y lo otro un valor
+// persona[propiedad] = "Pedro";//la variable propiedad se vuelve una propiedad como su nombre los dice y lo otro un valor
 
-console.log(persona);
+// console.log(persona);
 
 //editable writable
 //enumerable pertima listar
 //configurable permita eliminar
 
-Object.defineProperty(persona, "clan", {
-    value: 'Bezzos',//agragando un vlaor a la nueva propiedad clan
-    writable: false,//se puede cambiar
-    enumerable: false,//se puede buscar con keys para generar array con keys()
-    configurable: false//se puede eliminar
-});
+// Object.defineProperty(persona, "clan", {
+//     value: 'Bezzos',//agragando un vlaor a la nueva propiedad clan
+//     writable: false,//se puede cambiar
+//     enumerable: false,//se puede buscar con keys para generar array con keys()
+//     configurable: false//se puede eliminar
+// });
 
 //intentando cambiar
-persona.clan = "Bezozos";
+// persona.clan = "Bezozos";
 
-//eliminar
-delete persona.clan;
+// //eliminar
+// delete persona.clan;
 
 //delete persona.clan = "";//dejarlo vacio
 //delete persona.clan = undefined;//dejarlo undefined
 
 //mostrando
 
-console.log(persona);
+// console.log(persona);
 
 
-console.log(Object.keys(persona));//.KEYS() genera un array en cada propiedad
+// console.log(Object.keys(persona));//.KEYS() genera un array en cada propiedad
 
 // DELETE DE OBJETO
 /* Object.keys(persona).forEach(function (key) {
@@ -77,34 +77,190 @@ console.log(Object.keys(persona));//.KEYS() genera un array en cada propiedad
 })//Mostrar el array como una lista basica hacia abajo
 */
 
-Object.assign(persona, {estado: 'soltero', ciudad: 'Medellin'});//forma resumida de agregar varios
+// Object.assign(persona, {estado: 'soltero', ciudad: 'Medellin'});//forma resumida de agregar varios
 
-console.log(persona);
+// console.log(persona);
 
 
 // 2. Crear un elemento
 
-const parrafo = document.createElement("p");
-parrafo.textContent = "Hola, soy un parrafo";
+// const parrafo = document.createElement("p");
+// parrafo.textContent = "Hola, soy un parrafo";
 
 // 3. Añadirlo al documento
 
-document.body.appendChild(parrafo);
+// document.body.appendChild(parrafo);
 
 // 4. Insertarlo antes de otro elemento
 
-const contenedor = document.getElementById("contenedor");
+// const contenedor = document.getElementById("contenedor");
 /* 
 contenedor.insertAdjacentElement("beforebegin", parrafo); */
 
-console.log(`${persona.toString()}`);
-persona.toString()
+// console.log(`${persona.toString()}`);
+// persona.toString()
+
 //Metodo .toString()
-const number = 42;
-const boll = true;
-const regexp = /.+/;
+// const number = 42;
+// const boll = true;
+// const regexp = /.+/;
 
-console.log(number.toString());
-console.log(boll.toString());
-console.log(regexp.toString());
+// console.log(number.toString());
+// console.log(boll.toString());
+// console.log(regexp.toString());
 
+//JSON
+
+let persona = {
+    nombre: "Juan",
+    edad: 25,
+    ocupacion: "Programador",
+    hobbies: ['jugar', 'nadar', 'correr'],
+
+    //poniendo funcion
+    saludar: () =>{
+        return`hola soy ${this.nombre} y tengo ${this.edad} años.`;
+    },
+    toString: () =>{
+        return `persona: nombre: ${this.nombre}, "edad:" ${this.edad},
+        "ocupacion:" + ${this.ocupacion}`;
+    }//poninedo todo a string
+}
+
+const json = `{
+    "nombre":"Juan",
+    "edad":25,
+    "ocupacion":"Programador",
+    "hobbies":["jugar","nadar","correr"],
+    "direccion":{
+        "calle":"123 Calle principal",
+        "ciudad":"Ciudad Ejemplo",
+        "codigoPostal1": "123456"
+
+    }
+}`
+//json to string
+console.log(json);//json en string
+
+// let user = JSON.parse(json);//pasando json a string
+// console.log(user);
+
+// console.log(json.nombre);//string de json que no se puede manipular
+// console.log(user.nombre);//objeto del lenguaje manipulable
+
+//json to object
+
+// console.log(JSON.stringify(persona));
+// console.log(JSON.stringify(persona,null));//lo mismo que el de arriba pq no especificamos propiedad
+
+
+// console.log(JSON.stringify(persona,["nombre"]));//pasar de objeto a string para ver porpiedad especifica
+
+// console.log(JSON.stringify(persona,["nombre", "ocupacion"]));//pasar de objeto a string para ver 2 porpiedades especificas
+
+//Destructuracion de objetos
+//cambiando el valor desde fuera del objeto 
+let {hobbies, ocupacion} = persona;
+
+console.log(persona.ocupacion);
+console.log(ocupacion);
+
+
+ocupacion = "jugador";
+console.log(persona.ocupacion);
+console.log(ocupacion);
+
+persona.ocupacion = ocupacion;
+console.log(persona.ocupacion);
+
+//Crear una copia del objeto (superficial: copia referencia osea se modifican todos los objetos. profunda: ir al registro del objeto y crear uno completamente nuevo en otro registro, nose modifican entre ellos)
+
+//copia superficial
+
+const fullUser = {
+    ...persona,
+    power:25,
+    life: 58
+    
+}
+
+console.log("")
+console.log(persona);
+console.log(fullUser);
+console.log(persona.hobbies);
+console.log(fullUser.hobbies);
+
+fullUser.hobbies[0] = "program";//modificamos el primer hobbie
+console.log(fullUser.hobbies);
+console.log(persona.hobbies);
+
+//copia profunda
+
+
+
+//estructura anidada objetos
+
+const user = {
+    name: "Juan",
+    role: "streamer",
+    attributes: {
+        height: 183,
+        favColor: "blueviolet",
+        hairColor: "black"
+    }
+}
+
+console.log("")
+
+//accediendo a la estructura anidada
+
+// const {attributes} = user;
+// console.log(attributes);
+
+// let {attributes:{ hairColor }} = user;
+// console.log(hairColor);//solo accediendo
+
+// let {attributes:{ height:size }} = user;
+// size = 25;
+// console.log(size);//accediendo a altura para renombrarla
+
+// console.log("")
+// console.log(user);
+// console.log(user.role);
+// console.log(user.attributes);
+// console.log(user.attributes.favColor);
+
+//otra forma de crear objetos (clave, valor)
+const numeros = {
+    11: 1,
+    10: 2
+}
+console.log("")
+console.log(numeros[10])
+
+//iteradores de objetos, creando un objeto con los valores de un objeto, el resto despues de name es others
+console.log("")
+const {name, ...others} = user;
+console.log(others);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+//for in indice
+//for on valor
+
+//crea pare de clave valor
+const keys = ["name","life","power","talk"];
+const values = ["Juan", 99,
+10, () => {return "Hola"}];
+
+const entries = [];
+for (let i in values) {
+    const key = keys[i];
+    const value = values[i];
+    entries.push([key,value]);
+}
+
+const newUser = Object.fromEntries(entries);
+console.log(entries);
+console.log(newUser);
--- riwi/modulo3/semana2/talleres/Condicionales/script.js
+++ riwi/modulo3/semana2/talleres/Condicionales/script.js
@@ -1,7 +1,7 @@
 /* 1. Verifica si un número es positivo.  */
 /* 2. Verifica si un número es negativo.  */
 
-num = 7;
+num = 32;
 
 if (num >= 1 && num != 0) {
     console.log(`El numero ${num} es positivo`);
@@ -79,10 +79,10 @@ else{
 /* 10. Determina si un número es igual a 0.  */
 
     if (num === 0) {
-        console.log(`EL numero ${num} es cero :3!!!!!`);
+        console.log(`EL numero ${num} es cero`);
     }
     else{
-        console.log(`EL numero ${num} NO cero :3!!!!!`);
+        console.log(`EL numero ${num} NO es cero`);
 
     }
     
@@ -99,7 +99,7 @@ else{
     }
     
 /* 12. Determina si un número es un año bisiesto.  */
-    let año = 2024;
+    let año = 1990;
 
     if (año % 4 === 0 && año % 100 != 0 || año % 400 === 0 ) {
         console.log(`EL año ${año} es un año bisiesto`);
@@ -110,7 +110,7 @@ else{
 
 /* 13. Verifica si una persona es mayor de edad (mayor o igual a 18 años).  */
 
-let edadPersona = 12;
+let edadPersona = 50;
 
     if (edadPersona >= 18) {
         console.log(`su edad es ${edadPersona} osea SI ES MAYOR DE EDAD`);
@@ -160,28 +160,330 @@ const numFibo = 4;
 
 /* 18. Determina si un número es un palíndromo.  */
 
-    
+    let numPalindromo = [3,0,3];
+    let reversPalindromo = numPalindromo.slice().reverse();//[3,0,2] = [2,0,3]
+
+    console.log(numPalindromo);
+    console.log(reversPalindromo);
+
+
+    //si TODO valor e indice es igual entre numPalindromo y reversPalindromo
+    //retorna valor es igual al indice
+    //tenemos [3,0,2] y [2,0,3] SI en la posicion 0 el valor de reverse es 3 true sino false, Si en la posicion 1 el valor de reverse es el mismo de numPalindromo true sino false (TODOS DEBEN DAR TRUE) 
+
+    if (numPalindromo.every( (v,i) => {
+        return v === reversPalindromo[i] } ))
+    {
+        console.log(`El numero ${numPalindromo} SI ES UN PALINDROMO`);
+    }
+    else{
+        console.log(`El numero ${numPalindromo} NO ES UN PALINDROMO`);
+    }
+
 
 /* 19. Verifica si una cadena de texto contiene la palabra "JavaScript".  */
+
+let cadena = "JavaScript es un lenguaje de programacion 1.";
+
+if (cadena.toLowerCase().includes("javascript")) {
+    console.log("La cadena SI contiene la palabra JavaScript");
+} else {
+    console.log("La cadena NO contiene la palabra JavaScript");
+}
+
 /* 20. Determina si una cadena tiene más de 10 caracteres.  */
+
+    if (cadena.length > 10) {
+        console.log(`La cadena de texto: ${cadena} TIENE MAS DE 10 CARACTERES`);
+    }
+    else{
+        console.log(`La cadena de texto ${cadena} NO TIENE MAS DE 10 CARACTERES`);
+    }
+
 /* 21. Verifica si una cadena de texto está en minúsculas.  */
+
+    if (cadena.toLowerCase() == cadena) {
+        console.log(`La cadena de texto: ${cadena} ESTA EN MINUSCULAS`);
+    }
+    else{
+        console.log(`La cadena de texto: ${cadena} NO ESTA EN MINUSCULAS`);
+    }
+
 /* 22. Determina si una cadena de texto contiene al menos un número.  */
+
+    /*
+    / is used to denote the start and end of a regular expression in JavaScript.
+    \\d is used to match any digit (equivalent to [0-9]).
+    expression /\d/ will match any single digit in a string.
+     */
+    const cadenaTieneNumeros = /\d/.test(cadena);
+
+
+    if (cadenaTieneNumeros == true) {
+        console.log(`La candena de texto ${cadena} SI CONTIENE NUMEROS`);
+    }
+    else{
+        console.log(`La candena de texto ${cadena} NO CONTIENE NUMEROS`);
+    }
+
 /* 23. Verifica si una cadena de texto termina con un punto (.).  */
+
+    if (cadena.endsWith(".")) {
+        console.log(`La cadena: ${cadena} SI TERMINA CON PUNTO`)
+    }
+    else{
+        console.log(`La cadena: ${cadena} NO TERMINA CON PUNTO`)
+    }
+
 /* 24. Determina si una cadena de texto es un pangrama (contiene todas 
 las letras del alfabeto).  */
+
+    function esPangrama(frase) {
+        
+        const alfabeto =  "abcdefghijklmnopqrstuvwxyz";
+
+        const fraseMinuscula = frase.toLowerCase()        
+
+        let contar = 0;
+        for (const i in alfabeto) {
+            if (fraseMinuscula.includes(alfabeto[i])) {
+                contar += 1;
+            }
+        }
+
+        console.log(contar)
+
+        if (contar === 26) {
+            console.log(`La frase ${frase} SI ES UN PANGRAMA`)
+        }
+        else{
+            console.log(`La frase ${frase} NO ES UN PANGRAMA`)
+        }
+    }
+
+    const miFrase = "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja";
+
+    esPangrama(miFrase)
+
+    // function esPangrama(frase) {
+    //     const alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    //     const letrasFaltantes = [...alfabeto].filter(letra => !frase.toLowerCase().includes(letra));
+    //     return letrasFaltantes.length === 0;
+    // }
+
+    // const miFrase = "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja";
+    //  // Devuelve true si la frase es un pangrama, false si no lo es
+    // // console.log(esPangrama(miFrase)); // Devuelve true si la frase es un pangrama, false si no lo es
+
+    // if (esPangrama(miFrase) === true ) {
+    //     console.log(`La frase: (${miFrase}) SI ES UN PANGRAMA`);
+    // }
+    // else{
+    //     console.log(`La frase: (${miFrase}) NO ES UN PANGRAMA`);
+    // }
+    
 /* 25. Verifica si un día de la semana es laborable (de lunes a viernes).  */
+
+    const diaSemana = "MierColes";
+
+    switch (diaSemana.toLowerCase()) {
+        case "lunes":
+            console.log("el dia 'lunes' es un dia laborable");
+            break;
+        case "martes":
+            console.log("el dia 'martes' es un dia laborable");
+            break;
+        case "miercoles":
+            console.log("el dia 'miercoles' es un dia laborable");
+            break;
+        case "jueves":
+            console.log("el dia 'jueves' es un dia laborable");
+            break;
+        case "viernes":
+            console.log("el dia 'viernes' es un dia laborable");
+            break;
+        case "sabado":
+            console.log("el dia 'sabado' NO es un dia laborable");
+            break;
+        case "domingo":
+            console.log("el dia 'domingo' NO es un dia laborable");
+            break;
+    
+        default:
+            console.log("digito algo no valido...");
+            break;
+    }
+
 /* 26. Determina si un día es fin de semana (sábado o domingo).  */
+
+    if (diaSemana.toLowerCase() === "sabado" || diaSemana.toLowerCase() === "domingo") {
+        console.log(`El dia ${diaSemana} es fin de semana`);
+    }
+    else if (diaSemana.toLowerCase() === "lunes" || diaSemana.toLowerCase() === "martes" || diaSemana.toLowerCase() === "miercoles" || diaSemana.toLowerCase() === "jueves" || diaSemana.toLowerCase() === "viernes"){
+    console.log("¡Estas en semana!");
+    }
+    else{
+    console.log("digito algo no valido...");
+    }
+
 /* 27. Verifica si un número representa un mes válido (del 1 al 12).  */
+
+    const mes = 11;
+
+    if (mes >= 1 && mes <= 12 ) {
+        console.log(`El numero: ${mes} reresenta un mes del al (del 1 al 12)`);
+    }
+    else{
+        console.log("el numero que representa el mes es no valido");
+    }
+
 /* 28. Determina si una hora está en el rango de 9 AM a 6 PM.  */
+
+    function isHourInRange(hora) {
+        if (hora >= 9 && hora <= 21) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const hora = 6;//hora en modo 24 horas
+
+    if (isHourInRange(hora) === true) {
+        console.log(`La hora: ${hora} esta en el rango entre 9AM y 6PM`)
+    }
+    else{
+        console.log("La hora NO esta en el rango entre 9AM y 6PM")
+    }
+
 /* 29. Verifica si una persona es mayor de 65 años o menor de 18 años.  */
+
+    if (edadPersona >= 65) {
+        console.log(`Su edad es ${edadPersona} USTED ES MAYOR DE 65 AÑOS`);
+    }
+    else if (edadPersona <= 18){
+        console.log(`Su edad es ${edadPersona} USTED ES MENOR DE 18 AÑOS`);
+    }
+    else if (edadPersona <= 0){
+        console.log("Imposible tener menor de 0 años");
+    }
+    else{
+        console.log("No cumple ninguna condicion de la que se requiere su edad es no es ni mayor a 65 ni menor a 18");
+    }
+    
+
 /* 30. Determina si un triángulo es equilátero (tres lados iguales).  */
 /* 31. Verifica si un triángulo es isósceles (dos lados iguales).  */
 /* 32. Determina si un triángulo es escaleno (todos los lados diferentes).  */
+
+    let lado1 = 5;
+    let lado2 = 3;
+    let lado3 = 2;
+
+    if (lado1 === lado2 && lado2 === lado3) {
+        console.log(`Todos los lados son: ${lado1} ES EQUILATERO`);
+    }
+    else if(lado1 === lado2 && lado3 != lado2 || lado1 === lado3 && lado2 != lado3 || lado2 === lado3 && lado1 != lado3 ||  lado2 === lado1 && lado3 != lado1 ||  lado3 === lado1 && lado2 != lado1 ||  lado3 === lado2 && lado1 != lado1){
+        console.log(`dos lados valen ${lado1} y uno vale ${lado3} ES ISOCELES`);
+    }
+    else {
+        console.log(`Todos los lados son diferentes: ${lado1} ${lado2} ${lado3} ES ESCALENO`);
+    }
+    
+
 /* 33. Verifica si un número es mayor que el doble de otro número.  */
+
+    const numNormal = 4;
+    const numDoble = 8;
+    const dobleDeNumDoble = Math.pow(numDoble, 2);
+
+    if (numNormal > dobleDeNumDoble) {
+        console.log(`El numero ${numNormal} SI es mayor que el doble de ${numDoble}`);
+    }
+    else if (numNormal === dobleDeNumDoble){
+        console.log(`El doble de ${numDoble} ES IGUAL a el numero que estamos comparando que es: ${numNormal}`);
+    }
+    else{
+        console.log(`El numero ${numNormal} NO es mayor que el doble de ${numDoble}`);
+    }
+
 /* 34. Determina si la suma de dos números es mayor que 100.  */
+
+    const numSuma = num + numNormal;
+
+    if (numSuma > 100) {
+        console.log(`La suma de los numeros ${num} y ${numNormal} SI es mayor que 100`);
+    }
+    else{
+        console.log(`La suma de los numeros ${num} y ${numNormal} NO es mayor que 100`);
+    }
+
 /* 35. Verifica si la resta de dos números es menor que 50.  */
+
+    const numResta = num - numNormal;
+
+    if (numResta < 50) {
+        console.log(`La RESTA de los numeros ${num} y ${numNormal} SI es MENOR que 50`);
+    }
+    else{
+        console.log(`La RESTA de los numeros ${num} y ${numNormal} NO es MENOR que 50`);
+    }
+
 /* 36. Determina si un número es el doble del otro número.  */
+
+    if (numNormal === dobleDeNumDoble) {
+        console.log(`El numero ${numNormal} SI es el DOBLE de ${numDoble}`);
+    }
+    else{
+        console.log(`El numero ${numNormal} NO es el DOBLE de ${numDoble}`);
+    }
+
 /* 37. Verifica si el doble de un número es igual al triple de otro número.  */
+
+    const numTriple = 4;
+    const tripleDeNumTriple = Math.pow(numTriple, 3);
+
+    if (dobleDeNumDoble === tripleDeNumTriple) {
+        console.log(`El DOBLE del numero ${numDoble} SI es IGUAL al TRIPLE de ${numTriple}`);
+    }
+    else{
+        console.log(`El DOBLE del numero ${numDoble} NO es IGUAL al TRIPLE de ${numTriple}`);
+    }
+
 /* 38. Determina si la suma de dos números es igual a 50.  */
+
+    if (numSuma === 50) {
+        console.log(`La SUMA de los numeros ${num} y ${numNormal} SI es igual a 50`);
+    }
+    else{
+        console.log(`la suma de los numeros ${num} y ${numNormal} NO es igual a 50`);
+    }
+
 /* 39. Verifica si el producto de dos números es mayor que 500.  */
+    const numMutiplicacion = 5;
+    const MultiplicacionDeNumMultiplicacion = num * numMutiplicacion;
+
+    if (MultiplicacionDeNumMultiplicacion > 500) {
+        console.log(`La multiplicacion de los numeros ${num} y ${numMutiplicacion} SI es MAYOR a 500`);
+    }
+    else if(MultiplicacionDeNumMultiplicacion === 500){
+        console.log(`La multiplicacion de los numeros ${num} y ${numMutiplicacion} NO es IGUAL a lo que estamos comparando que es: 500`);
+    }
+    else{
+        console.log(`La multiplicacion de los numeros ${num} y ${numMutiplicacion} NO es MAYOR a 500`);
+    }
+
 /* 40. Determina si la división de dos números es menor que 10. */
+
+    const  numDivision = 4;
+    const divisionDeNumDivision = numDivision / num;
+
+    if (divisionDeNumDivision < 10) {
+        console.log(`La division de los numeros ${numDivision} y ${num} SI es MENOR a 10`)
+    }
+    else if (divisionDeNumDivision === 10){
+        console.log(`La division de los numeros ${numDivision} y ${num} es IGUAL al numero que estamos comparando que es: 10`)
+    }
+    else{
+        console.log(`La division de los numeros ${numDivision} y ${num} NO es MENOR a 10`)
+    }
