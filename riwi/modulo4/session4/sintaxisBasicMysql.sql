864fac17449a01176cf8718b2f179ea49404cdac
scorhenao <samcorhenao@gmail.com>
2024-06-18 17:56:49 -0400

    crud

--- riwi/modulo4/session4/sintaxisBasicMysql.sql
+++ riwi/modulo4/session4/sintaxisBasicMysql.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE BASEBASICADEDATOS;/*Crear base de datos*/
 /*Crear tablas dentro de la bd*/
+USE Auteco;
 CREATE TABLE ACTOR (
     id_actor int PRIMARY KEY,
     nombre VARCHAR(100)
@@ -12,3 +13,15 @@ INSERT INTO ACTOR VALUES
 (1, "samuel"),
 (2, "henao"),
 
+/*CRUD*/
+CREATE TABLE MOTOS (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL
+);
+
+INSERT INTO MOTOS VALUES ("F2");
+SELECT FROM MOTOS;
+DELETE FROM MOTOS WHERE id =2;
+UPDATE `MOTOS`
+SET nombre= "NKD"
+WHERE id = 1;
