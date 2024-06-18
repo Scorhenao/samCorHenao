0477174d8060bae6af0141d100b2035a5d096b75
scorhenao <samcorhenao@gmail.com>
2024-06-17 21:03:40 -0400

    insert tables

--- riwi/modulo4/session4/sintaxisBasicMysql.sql
+++ riwi/modulo4/session4/sintaxisBasicMysql.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE BASEBASICADEDATOS;/*Crear base de datos*/
+/*Crear tablas dentro de la bd*/
+CREATE TABLE ACTOR (
+    id_actor int PRIMARY KEY,
+    nombre VARCHAR(100)
+)
+/*Leer datos de una tabla*/
+SELECT * FROM ACTOR;
+
+/*Insertar datos en una tabla*/
+INSERT INTO ACTOR VALUES
+(1, "samuel"),
+(2, "henao"),
+
